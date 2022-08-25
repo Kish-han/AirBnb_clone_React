8c126ecb9417f0d85c1b12abe076c3ec79792954
Kishan <Kishanprasad09102001@gmail.com>
2022-08-25 13:31:55 +0530

    Banner added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Banner from '../components/Banner/Banner'
 import Header from '../components/Header/Header'
 import './Home.css'
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Header />
+            <Banner />
         </div>
     )
 }
